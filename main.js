0e534fd4657530878301370d484c8301f17e2447
TechnoShinobi <99727350+TechnoShinobi@users.noreply.github.com>
2022-04-25 17:36:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = ""
 song2 = ""
+rightWristX = 0
+leftWristY = 0
+rightWristY = 0
+leftWristX = 0
 function preload() {
 song1 = loadSound("music.mp3");
 song2 = loadSound("music2.mp3")
@@ -14,11 +18,27 @@ canvas = createCanvas(600,500);
 canvas.center();
 video = createCapture(VIDEO);
 video.hide();
-
-
-
+posenet = ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses);
 }
 
+function gotPoses(results) {
+if(results.length > 0)
+{
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+console.log(leftWristX);
+console.log(rightWristX);
+console.log(leftWristY);
+console.log(rightWristY);
+}
+}
+function modelLoaded(){
+console.log("Posenet Is Connected");
+}
 
 
 
