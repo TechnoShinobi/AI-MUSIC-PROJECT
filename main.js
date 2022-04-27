d01244a8b20d68fe4455e358c66a7d2d12978006
TechnoShinobi <99727350+TechnoShinobi@users.noreply.github.com>
2022-04-27 17:52:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = ""
 song2 = ""
+song1_status = ""
+song2_status = ""
+scoreleft = 0
+scoreright = 0
 rightWristX = 0
 leftWristY = 0
 rightWristY = 0
@@ -30,6 +34,8 @@ leftWristX = results[0].pose.leftWrist.x;
 leftWristY = results[0].pose.leftWrist.y;
 rightWristX = results[0].pose.rightWrist.x;
 rightWristY = results[0].pose.rightWrist.y;
+scoreleft = results[0].pose.keypoints[9].score;
+scoreright = results[0].pose.keypoints[10].score;
 console.log(leftWristX);
 console.log(rightWristX);
 console.log(leftWristY);
@@ -47,7 +53,17 @@ console.log("Posenet Is Connected");
 function draw() {
 image(video,0,0,600,500);
 
-
+fill("#FF0000")
+stroke("#FF0000")
+song2_status = song2.isPlaying();
+if (scoreleft > 0.2) {
+circle(leftWristX,leftWristY,20)
+song1.stop()
+if (song2_status == false) {
+song2.play();
+document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
+}
+}
 
 
 }
